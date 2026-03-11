ebce4d51c763462400aaf05b8380018a62aeaf66
Ruslan Taranov <taranov.rr@iturupbank.ru>
2026-03-11 18:15:04 +1100

    removed fake classes and reordered the file

--- src/definitions.ts
+++ src/definitions.ts
@@ -28,29 +28,79 @@ export const _MonoTypeStructDefinition = defineStruct({
   ]
 } as const);
 
-export const _MonoClassSizesStructDefinition = defineStruct({
-  name: "_MonoClassSizes",
-  fields: [
-    { ctype: "guint32", name: "class_size", type: "uint32" },
-    { ctype: "guint32", name: "element_size", type: "uint32" },
-    { ctype: "guint16", name: "native_size", type: "uint16" },
-    { ctype: "guint16", name: "static_field_size", type: "uint16" },
-    { ctype: "guint16", name: "generic_param_token", type: "uint16" },
-    { ctype: "guint32", name: "instance_size", type: "uint32" },
-    { ctype: "guint32", name: "instance_size_alloc", type: "uint32" }
-  ]
-} as const);
 
-export const _MonoPropertyBagStructDefinition = defineStruct({
-  name: "MonoPropertyBag",
-  fields: [
-    { ctype: "MonoPropertyBagItem", name: "head", type: "ptr" }
-  ]
-} as const);
+// struct _MonoClass {
+// 	MonoClass *element_class; 
+// 	MonoClass *cast_class; 
+// 	MonoClass **supertypes;
+// 	guint16     idepth;
+// 	guint8     rank;
+// 	guint8     class_kind;
+// 	guint inited          : 1;
 
+// 	/* ALL BITFIELDS SHOULD BE WRITTEN WHILE HOLDING THE LOADER LOCK */
+// 	guint size_inited     : 1;
+// 	guint valuetype       : 1; /* derives from System.ValueType */
+// 	guint enumtype        : 1; /* derives from System.Enum */
+// 	guint blittable       : 1; /* class is blittable */
+// 	guint unicode         : 1; /* class uses unicode char when marshalled */
+// 	guint wastypebuilder  : 1; /* class was created at runtime from a TypeBuilder */
+// 	guint is_array_special_interface : 1; /* gtd or ginst of once of the magic interfaces that arrays implement */
+// 	guint is_byreflike    : 1; /* class is a valuetype and has System.Runtime.CompilerServices.IsByRefLikeAttribute */
 
+// 	/* next byte */
+// 	guint8 min_align;
+// 	guint packing_size    : 4;
+// 	guint ghcimpl         : 1; /* class has its own GetHashCode impl */ 
+// 	guint has_finalize    : 1; /* class has its own Finalize impl */ 
+// 	guint marshalbyref    : 1; /* class is a MarshalByRefObject */
+// 	guint contextbound    : 1; /* class is a ContextBoundObject */
 
+// 	/* next byte */
+// 	guint delegate        : 1; /* class is a Delegate */
+// 	guint gc_descr_inited : 1; /* gc_descr is initialized */
+// 	guint has_cctor       : 1; /* class has a cctor */
+// 	guint has_references  : 1; /* it has GC-tracked references in the instance */
+// 	guint has_static_refs : 1; /* it has static fields that are GC-tracked */
+// 	guint no_special_static_fields : 1; /* has no thread/context static fields */
+// 	guint is_com_object : 1; 
+// 	guint nested_classes_inited : 1; /* Whenever nested_class is initialized */
 
+// 	/* next byte*/
+// 	guint interfaces_inited : 1; /* interfaces is initialized */
+// 	guint simd_type : 1; /* class is a simd intrinsic type */
+// 	guint has_finalize_inited    : 1; /* has_finalize is initialized */
+// 	guint fields_inited : 1; /* setup_fields () has finished */
+// 	guint has_failure : 1; /* See mono_class_get_exception_data () for a MonoErrorBoxed with the details */
+// 	guint has_weak_fields : 1; /* class has weak reference fields */
+// 	guint has_dim_conflicts : 1; /* Class has conflicting default interface methods */
+
+// 	MonoClass  *parent;
+// 	MonoClass  *nested_in;
+// 	MonoImage *image;
+// 	const char *name;
+// 	const char *name_space;
+// 	guint32    type_token;
+// 	int        vtable_size; /* number of slots */
+// 	guint16     interface_count;
+// 	guint32     interface_id;        /* unique inderface id (for interfaces) */
+// 	guint32     max_interface_id;
+// 	guint16     interface_offsets_count;
+// 	MonoClass **interfaces_packed;
+// 	guint16    *interface_offsets_packed;
+// 	guint8     *interface_bitmap;
+// 	MonoClass **interfaces;
+// 	union _MonoClassSizes sizes;
+// 	MonoClassField *fields;
+// 	MonoMethod **methods;
+// 	MonoType this_arg;
+// 	MonoType _byval_arg;
+// 	MonoGCDescriptor gc_descr;
+// 	MonoClassRuntimeInfo *runtime_info;
+// 	MonoMethod **vtable;
+// 	MonoPropertyBag infrequent_data;
+// 	void *unity_user_data;
+// };
 export const _MonoClassStructDefinition = defineStruct({
   name: "_MonoClass",
   fields: [
@@ -158,87 +208,3 @@ export const _MonoClassDefStructDefinition = defineStruct({
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// struct _MonoClass {
-// 	MonoClass *element_class; 
-// 	MonoClass *cast_class; 
-// 	MonoClass **supertypes;
-// 	guint16     idepth;
-// 	guint8     rank;
-// 	guint8     class_kind;
-// 	guint inited          : 1;
-
-// 	/* ALL BITFIELDS SHOULD BE WRITTEN WHILE HOLDING THE LOADER LOCK */
-// 	guint size_inited     : 1;
-// 	guint valuetype       : 1; /* derives from System.ValueType */
-// 	guint enumtype        : 1; /* derives from System.Enum */
-// 	guint blittable       : 1; /* class is blittable */
-// 	guint unicode         : 1; /* class uses unicode char when marshalled */
-// 	guint wastypebuilder  : 1; /* class was created at runtime from a TypeBuilder */
-// 	guint is_array_special_interface : 1; /* gtd or ginst of once of the magic interfaces that arrays implement */
-// 	guint is_byreflike    : 1; /* class is a valuetype and has System.Runtime.CompilerServices.IsByRefLikeAttribute */
-
-// 	/* next byte */
-// 	guint8 min_align;
-// 	guint packing_size    : 4;
-// 	guint ghcimpl         : 1; /* class has its own GetHashCode impl */ 
-// 	guint has_finalize    : 1; /* class has its own Finalize impl */ 
-// 	guint marshalbyref    : 1; /* class is a MarshalByRefObject */
-// 	guint contextbound    : 1; /* class is a ContextBoundObject */
-
-// 	/* next byte */
-// 	guint delegate        : 1; /* class is a Delegate */
-// 	guint gc_descr_inited : 1; /* gc_descr is initialized */
-// 	guint has_cctor       : 1; /* class has a cctor */
-// 	guint has_references  : 1; /* it has GC-tracked references in the instance */
-// 	guint has_static_refs : 1; /* it has static fields that are GC-tracked */
-// 	guint no_special_static_fields : 1; /* has no thread/context static fields */
-// 	guint is_com_object : 1; 
-// 	guint nested_classes_inited : 1; /* Whenever nested_class is initialized */
-
-// 	/* next byte*/
-// 	guint interfaces_inited : 1; /* interfaces is initialized */
-// 	guint simd_type : 1; /* class is a simd intrinsic type */
-// 	guint has_finalize_inited    : 1; /* has_finalize is initialized */
-// 	guint fields_inited : 1; /* setup_fields () has finished */
-// 	guint has_failure : 1; /* See mono_class_get_exception_data () for a MonoErrorBoxed with the details */
-// 	guint has_weak_fields : 1; /* class has weak reference fields */
-// 	guint has_dim_conflicts : 1; /* Class has conflicting default interface methods */
-
-// 	MonoClass  *parent;
-// 	MonoClass  *nested_in;
-// 	MonoImage *image;
-// 	const char *name;
-// 	const char *name_space;
-// 	guint32    type_token;
-// 	int        vtable_size; /* number of slots */
-// 	guint16     interface_count;
-// 	guint32     interface_id;        /* unique inderface id (for interfaces) */
-// 	guint32     max_interface_id;
-// 	guint16     interface_offsets_count;
-// 	MonoClass **interfaces_packed;
-// 	guint16    *interface_offsets_packed;
-// 	guint8     *interface_bitmap;
-// 	MonoClass **interfaces;
-// 	union _MonoClassSizes sizes;
-// 	MonoClassField *fields;
-// 	MonoMethod **methods;
-// 	MonoType this_arg;
-// 	MonoType _byval_arg;
-// 	MonoGCDescriptor gc_descr;
-// 	MonoClassRuntimeInfo *runtime_info;
-// 	MonoMethod **vtable;
-// 	MonoPropertyBag infrequent_data;
-// 	void *unity_user_data;
-// };
